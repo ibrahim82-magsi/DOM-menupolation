0dd522728ec989d50d2dae3cc026c68bc22f7942
ibrahim82-magsi <muhammad.ibrahim.magsi@gmail.com>
2025-06-16 12:21:28 +0500

    updat

--- script.js
+++ script.js
@@ -2,5 +2,8 @@
 
 // Select a paragraph with id "message" and change its text to "Welcome!"
 
- var heading = document.querySelector("#message");
- heading.innerHTML = "Welcome"
+//  var heading = document.querySelector("#message");
+//  heading.innerHTML = "Welcome"
+
+
+
